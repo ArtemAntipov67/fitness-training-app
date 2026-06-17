f195c44cced6d1e296b3c45b4155fc5fc03ffa74
ArtemAntipov67 <artem.antipov.666333@gmail.com>
2026-06-18 01:52:33 +0300

    Fix backend URL

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
     event.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://fitness-training-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
